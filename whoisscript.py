999d620406d7c0d2cf9902681acd7616a0a8b1ba
Lucas Turossi <75689046+secn0kk@users.noreply.github.com>
2022-11-06 16:12:29 -0300

    Update whoisscript.py

--- whoisscript.py
+++ whoisscript.py
@@ -1,8 +1,17 @@
 import whois
 import socket
 import dns.resolver
-import flask 
+from flask import Flask
 
+#Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+    
+if __name__ == "__whoisscript__":
+    app.run(debug=True)
 
 
 # defining a function that returns a boolean indicating whwther a 'domain_name' is registered or not.
@@ -34,10 +43,9 @@ getresolver = dns.resolver.Resolver()
 getns = getresolver.resolve(domain_name, "NS") 
 for rdata in getns:
     dnsrecords += str(rdata) + "\n"
-print("DNS utlizado pelo domínio: \n", dnsrecords)
+print("DNS utlizado pelo domínio:\n", dnsrecords)
 
 #Get entrada em A
-
 domainip = socket.gethostbyname(domain_name)
 print ("Apontamento em A domínio: \n", domainip)
 
@@ -46,14 +54,25 @@ print ("Apontamento em A domínio: \n", domainip)
 mailservers = "" 
 for x in dns.resolver.resolve(domain_name, 'MX'): 
     mailservers += x.to_text() + "\n"
-print("Entradas MX: \n", mailservers)
+    trata = ""
+    trata = mailservers.replace(" ","")
+    trata = trata.replace("10","")
+    trata = trata.replace("20","")
+    trata = trata.replace("0","")
+    trata = trata.replace("'","");  
+    print("Entradas MX: \n", str(trata.replace (" ","")))
+    spf = domain_name
+    domain_name = trata[:-2]
+    domain_name = socket.gethostbyname(domain_name)
+print ("Apontamento IP do MX : ",domain_name)
+
 
 #Get SPF and TXT entrys
 textrecords = ""
 getresolver = dns.resolver.Resolver()
     
 try:
-    gettext = getresolver.resolve(domain_name, "TXT") 
+    gettext = getresolver.resolve(spf, "TXT") 
     for rdata in gettext: 
         textrecords += str(rdata) + "\n"
 except:
